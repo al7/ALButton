fe79bb4b8c94e815e5d9a242c3e590412d1ec0e0
Alex Leite <admin@al7dev.com>
2015-02-22 19:13:42 -0800

    Control implementation

--- ALButton/Source/ALButton.swift
+++ ALButton/Source/ALButton.swift
@@ -1,24 +1,59 @@
-//
-//  ALButton.swift
-//  ALButton
-//
-//  Created by Alexandre Leite on 2/22/15.
-//  Copyright (c) 2015 Alexandre Leite. All rights reserved.
-//
+/*
+
+Copyright (c) 2015 - Alex Leite (al7dev)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
 
 import UIKit
 
-public typealias ButtonStyle = (UIFont, UIColor, UIColor, UIColor)
-public typealias ButtonHandler = (sender: UIButton) -> Void
+public typealias ButtonHandler = (UIButton) -> Void
 
 public class ALButton: UIButton {
     
+    private typealias KVOContext = UInt8
+    private var context = KVOContext()
+
     private var touchUpInsideHandlers: [ButtonHandler] = []
+    private var style: ButtonStyle = ButtonStyle()
+    private var touchingDown = false
     
     //MARK- Initializers
     
-    public init(title: String!, style: Style) {
+    public init(title: String!, style: ButtonStyle? = nil, touchUpHandler: ButtonHandler? = nil) {
         super.init(frame: CGRectZero)
+        
+        self.setTitle(title, forState: .Normal)
+        if let st = style {
+            self.style = st
+        }
+        
+        self.updateFace()
+        
+        self.addTarget(self, action: "onBaseTouchUp:", forControlEvents: .TouchUpInside)
+        self.addTarget(self, action: "onBaseTouchUp:", forControlEvents: .TouchUpOutside)
+        self.addTarget(self, action: "onBaseTouchDown:", forControlEvents: .TouchDown)
+        self.addTarget(self, action: "fireTouchUpInsideHandlers:", forControlEvents: .TouchUpInside)
+        
+        self.addObserver(self, forKeyPath: "enabled", options: .New | .Old, context: &context)
+        self.addObserver(self, forKeyPath: "style", options: .New | .Old, context: &context)
     }
 
     required public init(coder aDecoder: NSCoder) {
@@ -27,17 +62,75 @@ public class ALButton: UIButton {
     
     //MARK- Base Overrides
     
+    public override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        if keyPath == "enabled" {
+            self.updateFace()
+        }
+    }
+    
+    deinit {
+        self.removeObserver(self, forKeyPath: "enabled")
+        self.removeObserver(self, forKeyPath: "style")
+    }
     
     //MARK- Public Methods
     
     public func touchUpInside(handler: ButtonHandler!) {
-        touchUpInsideHandlers.append(handler)
+        self.touchUpInsideHandlers.append(handler)
     }
     
     public func clearTouchUpInsideHandlers() {
-        touchUpInsideHandlers = []
+        self.touchUpInsideHandlers = []
     }
     
     //MARK- Private Methods
     
+    public func updateFace() {
+        //-- Setting fixed color values
+        self.setTitleColor(self.style.titleColor,  forState: .Normal)
+        self.setTitleColor(self.style.disabledTitleColor, forState: .Disabled)
+        
+        //-- Setting variable color values
+        if self.enabled {
+            if self.touchingDown {
+                self.backgroundColor = self.style.highlightColor
+            }
+            else {
+                self.backgroundColor = self.style.backgroundColor
+            }
+        }
+        else {
+            self.backgroundColor = self.style.disabledColor
+        }
+    }
+    
+    //MARK- Action Targets
+    
+    internal func onBaseTouchUp(sender: UIButton) {
+        self.touchingDown = false
+        self.updateFace()
+    }
+    
+    internal func onBaseTouchDown(sender: UIButton) {
+        self.touchingDown = true
+        self.updateFace()
+    }
+    
+    internal func fireTouchUpInsideHandlers(sender: UIButton) {
+        for handler in self.touchUpInsideHandlers {
+            handler(self)
+        }
+    }
+    
+    //MARK- Helper Structs
+    
+    public struct ButtonStyle {
+        public var titleFont = UIFont.systemFontOfSize(14.0)
+        public var titleColor = UIColor.whiteColor()
+        public var disabledTitleColor = UIColor.lightGrayColor()
+        public var backgroundColor = UIColor(red: 0, green: 204.0 / 255.0, blue: 0.0, alpha: 1.0)
+        public var highlightColor = UIColor(red: 0.0, green: 153.0 / 255.0, blue: 0.0, alpha: 1.0)
+        public var disabledColor = UIColor.grayColor()
+    }
+
 }
--- ALButton/ViewController.swift
+++ ALButton/ViewController.swift
@@ -15,6 +15,42 @@ class ViewController: UIViewController {
     override func loadView() {
         self.view = UIView(frame: UIScreen.mainScreen().bounds)
         self.view.backgroundColor = UIColor.whiteColor()
+        
+        let enableButton = ALButton(title: "Enable")
+        enableButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.view.addSubview(enableButton)
+        
+        let disableButton = ALButton(title: "Disable")
+        disableButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.view.addSubview(disableButton)
+        
+        let views = ["enableButton": enableButton, "disableButton": disableButton]
+        
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[enableButton]-[disableButton]-|", options: NSLayoutFormatOptions(0), metrics: nil, views: views))
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[enableButton(44)]", options: NSLayoutFormatOptions(0), metrics: nil, views: views))
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[disableButton(44)]", options: NSLayoutFormatOptions(0), metrics: nil, views: views))
+        self.view.addConstraint(NSLayoutConstraint(item: enableButton, attribute: .CenterY, relatedBy: .Equal, toItem: enableButton.superview, attribute: .CenterY, multiplier: 1, constant: 0))
+        self.view.addConstraint(NSLayoutConstraint(item: disableButton, attribute: .CenterY, relatedBy: .Equal, toItem: disableButton.superview, attribute: .CenterY, multiplier: 1, constant: 0))
+        self.view.addConstraint(NSLayoutConstraint(item: disableButton, attribute: .Width, relatedBy: .Equal, toItem: enableButton, attribute: .Width, multiplier: 1, constant: 0))
+        
+        
+        enableButton.touchUpInside {
+            sender in
+            
+            if disableButton.enabled {
+                let alertView = UIAlertView(title: "Button Touched", message: "Disable Button is already enabled", delegate: nil, cancelButtonTitle: "Ok")
+                alertView.show()
+            }
+            else {
+                disableButton.enabled = true
+            }
+        }
+        
+        disableButton.touchUpInside {
+            sender in
+            
+            disableButton.enabled = false
+        }
     }
     
     override func viewDidLoad() {
@@ -26,8 +62,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
 
     }
-    
-
 
 }
 
